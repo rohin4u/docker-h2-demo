869645b77aa5248e3fe217510611ef2762858bb8
rohin4u <rohinmaleyawr@gmail.com>
2023-10-06 13:08:53 +0530

    created docker-mysql project

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into employee
+(employee_id,employee_name,employee_address,employee_department)
+values('s201','Martin Lucas','WorkShire, WA', 'Mechanics');
+insert into employee
+(employee_id,employee_name,employee_address,employee_department)
+values('s202','Alex Burman','New Crox House, PA', 'Chemics');
+insert into employee
+(employee_id,employee_name,employee_address,employee_department)
+values('s203','DonkBos','The New Sqare', 'IroOre');
